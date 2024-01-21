9e13e6ff8fb4648ec579d1bbb00e2191b5256054
Jereminet <jeremy.minet.97@hotmail.com>
2024-01-21 19:51:56 +0100

    oupsi j'ai fait une betise le B

--- common.R
+++ common.R
@@ -7,7 +7,7 @@ send_mail <- function(subject, payload) {
     ### YAML IMPORT AND USEFUL VALUES DEFINITION ==================================
     params    <- yaml.load_file("params.yaml")
     sender    <- params$sender
-    targets   <- params$recipients@
+    targets   <- params$recipients
     pw        <- Sys.getenv("MAIL_PWD")
     smtp      <- params$outlook_smtp
     smtp_info <- list(host.name = smtp, port = 587, user.name = sender, passwd = pw, tls = TRUE)
